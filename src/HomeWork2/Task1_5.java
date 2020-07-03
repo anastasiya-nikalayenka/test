20c335244998a5683e77e8dc45a6c7a1d6dc5361
Anastasiya Nikalayenka <anastasiya.nikalayenka@gmail.com>
2020-07-03 23:25:28 +0300

    second commit

--- src/HomeWork2/Task1_5.java
+++ src/HomeWork2/Task1_5.java
@@ -3,8 +3,13 @@ package HomeWork2;
 public class Task1_5 {
     public static void main(String[] args) {
         long a = 1;
-        for(int i=0; i<1000000000; i++) {
+        long maxValue = 9223372036854775807L;
+        for(; ;) {
             a *= 3;
+            if (a>=maxValue) {
+                System.out.println("limit!" + a);
+                break;
+            }
         }
         System.out.println(a);
     }
