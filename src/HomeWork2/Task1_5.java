42cd816a76b2f9a18b203b614951fd5b7b0691d6
Anastasiya Nikalayenka <anastasiya.nikalayenka@gmail.com>
2020-07-04 18:02:21 +0300

    Task 1_5 was edited

--- src/HomeWork2/Task1_5.java
+++ src/HomeWork2/Task1_5.java
@@ -3,14 +3,21 @@ package HomeWork2;
 public class Task1_5 {
     public static void main(String[] args) {
         long a = 1;
-        long maxValue = 9223372036854775807L;
-        for(; ;) {
-            a *= 3;
-            if (a>=maxValue) {
-                System.out.println("limit!" + a);
-                break;
+        long value = 0;
+        long[] arr = new long[100];
+
+        for (; ; ) {
+            while (a <= Long.MAX_VALUE || a > 0) {
+                for (int i = 0; i < 100; i++) {
+                    a *= 3;
+                    arr[i] = a;
+                    if (a >= Long.MAX_VALUE || a < 0) {
+                        System.out.println(arr[i-1]);
+                        System.out.println(arr[i]);
+                        break;
+                    }
+                }
             }
         }
-        System.out.println(a);
     }
 }
